2e4031bc2fbbf17982f99ecb5707e68b5d297780
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-10-01 11:39:25 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-console.log("ola")
+let setTime = 1000; 
+
+let valorMinuto = 0;
+let valorSegundo = 0;
+const valorTela = document.getElementById("clock");
+contador()
+
+
+
+function contador(){
+    setTimeout(()=>{
+        if(valorSegundo<61){
+            if(valorMinuto.toString().length == 2){
+                unidadeMinutos();
+                contador()
+            }else{
+                unidadeSegundos();
+                contador()
+            }
+        }  
+        if(valorSegundo == 60){
+            resetSegundos();
+        }
+    },setTime)
+}
+
+function addTela(val){
+    valorTela.innerHTML = val;
+    return; 
+}
+
+
+
+function unidadeSegundos(){
+    switch(valorSegundo.toString().length){
+        case 1: 
+            addTela(`0${valorMinuto}:0${valorSegundo}`)
+            return valorSegundo++;
+        case 2: 
+            addTela(`0${valorMinuto}:${valorSegundo}`)
+            return valorSegundo++;
+    }
+}
+function unidadeMinutos(){
+    switch(valorSegundo.toString().length){
+        case 1: 
+            addTela(`${valorMinuto}:0${valorSegundo}`)
+            return valorSegundo++;
+        case 2: 
+            addTela(`${valorMinuto}:${valorSegundo}`)
+            return valorSegundo++;
+    }
+}
+function resetSegundos(){
+    return  valorMinuto++, valorSegundo = 0; 
+}
